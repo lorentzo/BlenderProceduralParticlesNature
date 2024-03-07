336a9d7555ab015868b4eb184064d64a9a219fe6
Lovro Bosnar <lovro.bosnar@itwm.fraunhofer.de>
2024-03-07 09:59:07 +0100

    core update

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# BlenderProceduralParticlesNature
+# Blender Procedural Particles Nature
+
+WIP
+
+Blender 4.0.2
+
+## Art concepts:
+* Small planet with growing plants (spring)
+* Particles flying while plants growing
+
+## Techncal Concepts:
+* Using multiple hair particle systems
+* Using Blender Python API to procedurally generate vertex weights (see script)
+* Using generated vertex weights to control hair particle system density and scale
+* Using emitter particle system with turbulence force field
+
+## Gallery
+
+![](Gallery/1.png)
+![](Gallery/2.png)
+![](Gallery/3.png)
+![](Gallery/4.png)
+![](Gallery/5.png)
+
+## TODO:
+* More hair particles systems for plants
+* Enhance hair particle system growth
+* enhance flying particles shape
+* light and cam
+
+## Ideas
+* rendering as sprites for 2D game in unity
+* vertex weights can not be key-framed and thus animated. Extend this concept in Houdini
---
+++ procedural.py
@@ -0,0 +1,62 @@
+
+import bpy
+import mathutils
+
+# Vertex groups are created per object.
+# Each vertex group element can be read/write from object.
+# Each vertex group element can be read from vertex.
+# bpy.types.object.vertex_groups (https://docs.blender.org/api/current/bpy.types.Object.html#bpy.types.Object.vertex_groups)
+# bpy.types.VertexGroups (https://docs.blender.org/api/current/bpy.types.VertexGroups.html)
+# bpy.types.VertexGroup https://docs.blender.org/api/current/bpy.types.VertexGroup.html
+# bpy.types.Object.data -> bpy.types.mesh (if object contains mesh) https://docs.blender.org/api/current/bpy.types.Object.html#bpy.types.Object.data
+# bpy.types.mesh.vertices (https://docs.blender.org/api/current/bpy.types.Mesh.html#bpy.types.Mesh.vertices)
+# bpy.types.MeshVertex.groups https://docs.blender.org/api/current/bpy.types.MeshVertex.html#bpy.types.MeshVertex.groups
+# bpy.types.VertexGroupElement https://docs.blender.org/api/current/bpy.types.VertexGroupElement.html#bpy.types.VertexGroupElement
+
+for obj in bpy.data.collections["ProceduralVertWeight"].all_objects:
+    nVertGroupsToCreate = len(obj.particle_systems) # equals to the number of particle systems!
+    print("Checking if object has existing vertex groups...")
+    # bpy_struct.items() returns the items of this objects custom properties (matches Python’s dictionary function of the same name).
+    # https://docs.blender.org/api/current/bpy.types.bpy_struct.html#bpy.types.bpy_struct.items
+    if len(obj.vertex_groups.items()) <= 0:
+        print("Object contains no vertex groups!")
+    else:
+        print("Object contains vertex groups! Removing existing vertex groups!", len(obj.vertex_groups.items()))
+        existing_vertex_groups = []
+        for vg_idx in range(len(obj.vertex_groups.items())):
+            existing_vertex_groups.append(obj.vertex_groups[vg_idx])
+        while existing_vertex_groups:
+            obj.vertex_groups.remove(existing_vertex_groups.pop())
+    
+    print("Creating new vertex groups...")
+    for i in range(nVertGroupsToCreate):
+        group = obj.vertex_groups.new( name = str(i) )
+        print("Created vertex group", str(i))
+
+    print("Populating vertex groups...")
+    for vert in obj.data.vertices:
+        v_co = vert.co
+        v_groups = vert.groups # read only
+        v_idx = vert.index
+        for vg in obj.vertex_groups:
+            vg_idx = vg.index
+            # https://docs.blender.org/api/current/mathutils.noise.html
+            H = 1.2
+            lacunarity = 2 + int(vg_idx)
+            octaves = 2 + int(vg_idx)
+            vert_weight = mathutils.noise.multi_fractal(v_co, H, lacunarity, octaves, noise_basis='PERLIN_ORIGINAL') * 0.9
+            vg.add([v_idx], vert_weight, "REPLACE")
+            print("For vertex", v_idx, "populating group", vg_idx, "with", vert_weight)
+
+    print("Updating particle systems...")
+    i = 0
+    for ps in obj.particle_systems:
+        ps.vertex_group_length = str(i)
+        ps.vertex_group_density = str(i)
+        i += 1
+
+            
+            
+
+        
+        
